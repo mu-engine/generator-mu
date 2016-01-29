27168725221a0e124c556cc12a201e7353716ded
John J. Glynn IV <jjg1914@rit.edu>
2016-01-28 22:40:03 -0500

    Improve state signature

--- state/templates/_state.js
+++ state/templates/_state.js
@@ -1,7 +1,7 @@
 import Mu from "mu-engine";
 
-export default function <%= name.constant %>State() {
-  return function(engine, state) {
+export default function <%= name.constant %>State(engine, state) {
+  return [ engine, state.unshift(function(engine, state) {
     return Mu.chainSystems(engine, state, []);
-  };
+  }) ];
 };
